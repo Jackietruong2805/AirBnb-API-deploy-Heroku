e07533aba51ebd17bc84da822cf08a8892661253
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-14 08:57:58 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const rootRoutes = require('./Routes');
-const port = process.env.PORT || 3333;
+const port = 3333;
 
 app.use(cors());
-app.use(express.static("./public/img"));
+app.use(express.static("."));
 app.use(express.json());
 
 app.use('/api', rootRoutes);
