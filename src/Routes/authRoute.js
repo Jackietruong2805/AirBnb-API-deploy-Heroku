79c5501a1719d5fb6eccd7b7d5121c31c86661e5
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-20 19:58:54 +0700

    deploy heroku

--- src/Routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authRoute = express.Router();
-const {signUp, signIn} = require('../Controllers/authController');
-
-authRoute.post('/auth/signup', signUp);
-authRoute.post('/auth/signin', signIn);
-
-module.exports = authRoute;
